78363c3e542a0a341deb7a360e920ccb643d5615
michaeloverman <overman.michael@gmail.com>
2016-05-26 08:17:15 -0400

    more of the same

--- mobile/src/main/java/tech/michaeloverman/android/mywatch/MyWatchConfigActivity.java
+++ mobile/src/main/java/tech/michaeloverman/android/mywatch/MyWatchConfigActivity.java
@@ -1,14 +1,18 @@
 package tech.michaeloverman.android.mywatch;
 
 import android.app.Activity;
+import android.content.ComponentName;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.wearable.companion.WatchFaceCompanion;
+import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.wearable.DataApi;
+import com.google.android.gms.wearable.Wearable;
 
 /**
  * Created by Michael on 5/26/2016.
@@ -18,17 +22,47 @@ public class MyWatchConfigActivity extends Activity
         GoogleApiClient.OnConnectionFailedListener,
         ResultCallback<DataApi.DataItemResult> {
 
+    private String watchFacePeerId;
+    private ComponentName mComponentName;
+    private GoogleApiClient myGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mywatch_config);
+        TextView title = (TextView) findViewById(R.id.title);
         watchFacePeerId = getIntent().getStringExtra(WatchFaceCompanion.EXTRA_PEER_ID);
+        mComponentName = getIntent().getParcelableExtra(WatchFaceCompanion.EXTRA_WATCH_FACE_COMPONENT);
+
+        myGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(Wearable.API)
+                .build();
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        myGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        if (myGoogleApiClient != null && myGoogleApiClient.isConnected()) {
+            myGoogleApiClient.disconnect();
+        }
+        super.onStop();
     }
 
     @Override
     public void onConnected(@Nullable Bundle bundle) {
-
+        if (watchFacePeerId != null) {
+            // do these things
+        } else {
+            // Do this if there's no connection
+        }
     }
 
     @Override
---
+++ wear/src/main/java/tech/michaeloverman/android/mywatch/MyWearableConfigActivity.java
@@ -0,0 +1,10 @@
+package tech.michaeloverman.android.mywatch;
+
+import android.app.Activity;
+
+/**
+ * Created by Michael on 5/26/2016.
+ */
+public class MyWearableConfigActivity extends Activity {
+
+}
